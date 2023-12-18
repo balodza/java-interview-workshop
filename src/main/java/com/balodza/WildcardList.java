85268b847c32047f8d5ad209352b1b14bf1e1d78
vstadnyk <vstadnyk@apple.com>
2023-12-18 15:05:21 +0200

    added widlcard collection test

---
+++ src/main/java/com/balodza/WildcardList.java
@@ -0,0 +1,28 @@
+package com.balodza;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WildcardList {
+    public static void main(String[] args) {
+        List<? extends A> list = new ArrayList<>();
+        list.add(new A());
+        List<A> casted = (List<A>) list;
+        casted.add(new A());
+        casted.add(new B());
+        casted.add(new C());
+        f(casted);
+        f(list);
+    }
+    
+    public static void f(List<? extends A> list) {
+        list.add(new A());
+    }
+    
+    static class A {}
+    
+    static class B extends A {}
+
+    static class C extends B {}
+    
+}
