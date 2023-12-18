09bd032f4148ba4e04f47539a4337488351c2e70
vstadnyk <vstadnyk@apple.com>
2023-12-18 16:52:40 +0200

    How to lost element in map

--- src/main/java/com/balodza/MapLostValue.java
+++ src/main/java/com/balodza/MapLostValue.java
@@ -2,6 +2,7 @@ package com.balodza;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * Mutable key leads to elements lost.
@@ -9,11 +10,12 @@ import java.util.Map;
 public class MapLostValue {
     public static void main(String[] args) {
         Map<MapKey, String> map = new HashMap<>();
-        MapKey key1 = new MapKey(123);
-        MapKey key2 = new MapKey(256);
+        MapKey key1 = new MapKey(123, "key 1");
+        MapKey key2 = new MapKey(256, "key 2");
         map.put(key1, "key1");
         map.put(key2, "key2");
         key1.setKey(100);
+        key2.setValue("key 3");
         System.out.println("get(key1): " + map.get(key1));
         System.out.println("get(key2): " + map.get(key2));
         System.out.println("values in map: " + map.values());
@@ -21,9 +23,11 @@ public class MapLostValue {
     
     static class MapKey {
         private int key;
+        private String value;
 
-        public MapKey(int key) {
+        public MapKey(int key, String value) {
             this.key = key;
+            this.value = value;
         }
 
         public int getKey() {
@@ -34,8 +38,28 @@ public class MapLostValue {
             this.key = key;
         }
 
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
         public int hashCode() {
             return key;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (!(o instanceof MapKey mapKey)) {
+                return false;
+            }
+
+            return Objects.equals(value, mapKey.value);
         }
     }
 }
