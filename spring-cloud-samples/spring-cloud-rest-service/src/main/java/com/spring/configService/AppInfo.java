c11d24ea8fcc904a2afe6fdf363ac9f93b97bb04
vstadnyk <vstadnyk@apple.com>
2023-12-21 10:19:26 +0200

    Changed data and structure

---
+++ spring-cloud-samples/spring-cloud-rest-service/src/main/java/com/spring/configService/AppInfo.java
@@ -0,0 +1,22 @@
+package com.spring.configService;
+
+public class AppInfo {
+    public String name;
+    public String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ spring-cloud-samples/spring-cloud-rest-service/src/main/java/com/spring/configService/EurekaClientApplication.java
@@ -0,0 +1,32 @@
+package com.spring.configService;
+
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class EurekaClientApplication {
+
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+
+    @RequestMapping("/greeting")
+    public String greeting() {
+        return eurekaClient.getApplication(appName).getName();
+    }
+}
